77cae4cac0e41155e05e127ff2d7faec6c711677
Michael Alan Stowe <mastowe94@gmail.com>
2023-11-11 17:07:45 -0600

    will not work for all words but does for these

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+class Anagram:
+
+    def __init__(self,word):
+        self.word = word
+        self.list = list()
+    
+    def match(self,word_list):
+        for w in word_list:
+            match = True
+            for letter in w:
+                if letter in self.word:
+                    match = True
+                else:
+                    match = False
+                    break
+            if match == True:
+                self.list.append(w)
+        return self.list
